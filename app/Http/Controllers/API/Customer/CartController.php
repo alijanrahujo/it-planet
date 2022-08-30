f477f06a047f01c2f9975c02497794188a339508
oratecjamshoro <oratec.edu@gmail.com>
2022-08-30 21:13:25 +0500

    cart delete item

--- app/Http/Controllers/API/Customer/CartController.php
+++ app/Http/Controllers/API/Customer/CartController.php
@@ -15,12 +15,12 @@ class CartController extends Controller
 {
     public function add_to_cart(Request $request)
     {
-        $product_id = $request->product_id;
-        $product_qty = $request->product_qty;
-        $user_id = auth('sanctum')->user()->id;
-
         if(auth('sanctum')->check())
         {
+            $product_id = $request->product_id;
+            $product_qty = $request->product_qty;
+            $user_id = auth('sanctum')->user()->id;
+
             $product = Product::where('id',$product_id)->first();
             if($product)
             {
@@ -56,6 +56,72 @@ class CartController extends Controller
                     'status' => 1,
                     'message' => 'Product Not Found',
                 ]);
+            }
+        }
+        else
+        {
+            return response()->json([
+                'status_code' => 401,
+                'status' => 1,
+                'message' => 'Login to add to cart',
+              ]);
+        }
+    }
+    public function getcart()
+    {
+        if(auth('sanctum')->check())
+        {
+            $user_id = auth('sanctum')->user()->id;
+            $cart = CartApi::where('user_id',$user_id)->get();
+            if($cart)
+            {
+                return response()->json([
+                    'status_code' => 200,
+                    'status' => 1,
+                    'data' => $cart,
+                ]);
+            }
+            else
+            {
+                return response()->json([
+                    'status_code' => 200,
+                    'status' => 1,
+                    'message' => 'Cart is empty',
+                ]); 
+            }
+        }
+        else
+        {
+            return response()->json([
+                'status_code' => 401,
+                'status' => 1,
+                'message' => 'Login to add to cart',
+              ]);
+        }
+    }
+
+    public function delete_item($id)
+    {
+        if(auth('sanctum')->check())
+        {
+            $user_id = auth('sanctum')->user()->id;
+            if(CartApi::where(['user_id'=>$user_id,'id'=>$id])->exists())
+            {
+                CartApi::where(['user_id'=>$user_id,'id'=>$id])->delete();
+                
+                return response()->json([
+                    'status_code' => 200,
+                    'status' => 1,
+                    'message' => 'Successfully deleted from cart',
+                ]);
+            }
+            else
+            {
+                return response()->json([
+                    'status_code' => 200,
+                    'status' => 1,
+                    'message' => 'Item Not Found',
+                ]); 
             }
         }
         else
