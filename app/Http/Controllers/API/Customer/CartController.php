8aa2ef4f6aeb111cae9f0ab4528b4eacc3be3b4a
oratecjamshoro <oratec.edu@gmail.com>
2022-08-30 17:14:03 +0500

    add to cart

--- app/Http/Controllers/API/Customer/CartController.php
+++ app/Http/Controllers/API/Customer/CartController.php
@@ -9,27 +9,45 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 use App\Models\Product;
+use App\Models\CartApi;
 
 class CartController extends Controller
 {
     public function add_to_cart(Request $request)
     {
+        $product_id = $request->product_id;
+        $product_qty = $request->product_qty;
+        $user_id = auth('sanctum')->user()->id;
+
         if(auth('sanctum')->check())
         {
-            $product = Product::where('id',$request->product_id)->first();
+            $product = Product::where('id',$product_id)->first();
             if($product)
             {
-                $data = [
-                    "product_id" => $request->product_id,
-                    "product_qty" => $request->product_qty,
-                    "user_id" => auth('sanctum')->user()->id
-                ];
-        
-                return response()->json([
-                    'status_code' => 200,
-                    'status' => 1,
-                    'message' => $data,
-                ]);
+                if(CartApi::where('user_id',$user_id)->where('product_id',$product_id)->exists())
+                {
+
+                    CartApi::where(['user_id'=>$user_id,'product_id'=>$product_id])->update(['product_qty'=>$product_qty]);
+                    return response()->json([
+                        'status_code' => 200,
+                        'status' => 1,
+                        'message' => 'Successfully updated to cart',
+                    ]);
+                }
+                else
+                {
+                    $cartitem = new CartApi;
+                    $cartitem->user_id = $user_id;
+                    $cartitem->product_id = $product_id;
+                    $cartitem->product_qty = $product_qty;
+                    $cartitem->save();
+
+                    return response()->json([
+                        'status_code' => 200,
+                        'status' => 1,
+                        'message' => 'Successfully added to cart',
+                    ]);    
+                }
             }
             else
             {
